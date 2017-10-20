dc3b02d46df15a33c969419d9e795bfc12d9be99
Zachary Marston <zmarston@live.com>
2017-10-20 12:40:08 -0700

    add style for form

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
   $("form#quiz").submit(function(event){
-  event.preventDefault();
-  var q1 = $("#q1").val();
-  var q2 = $("#q2").val();
-  var q3 = $("#q3").val();
-  var q4 = $("#q4").val();
-  var q5 = $("#q5").val();
+    event.preventDefault();
+    var q1 = $("#q1").val();
+    var q2 = $("#q2").val();
+    var q3 = $("#q3").val();
+    var q4 = $("#q4").val();
+    var q5 = $("#q5").val();
 
-  if (q1 === "css" && q5 === "css" || q1 === "css" && q2 === "css" || q5 === "css" && q2 === "css" || q5 === "css" && q3 === "css"){
-    $("#cssimage").show();
-    $("#javaimage, #csharpimage").hide();
-  } else if (q1 === "java" && q5 === "java" || q1 === "java" && q3 === "java" || q5 === "java" && q2 === "java" || q5 === "java" && q3 === "java"){
-    $("#javaimage").show();
-    $("#cssimage, #csharpimage").hide();
-  } else {
-    $("#csharpimage").show();
-    $("#javaimage, #cssimage").hide();
-  }
+    if (q1 === "css" && q5 === "css" || q1 === "css" && q2 === "css" || q5 === "css" && q2 === "css" || q5 === "css" && q3 === "css"){
+      $("#cssimage").show();
+      $("#javaimage, #csharpimage, #quiz").hide();
+    } else if (q1 === "java" && q5 === "java" || q1 === "java" && q3 === "java" || q5 === "java" && q2 === "java" || q5 === "java" && q3 === "java"){
+      $("#javaimage").show();
+      $("#cssimage, #csharpimage, #quiz").hide();
+    } else {
+      $("#csharpimage").show();
+      $("#javaimage, #cssimage, #quiz").hide();
+    }
   });
 });
